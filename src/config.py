d5c194fea7dc836005f9738f3464d2fc0489b580
Addy-codes <adeeb9839@gmail.com>
2024-09-15 17:21:27 +0530

    Add: basic description

--- src/config.py
+++ src/config.py
@@ -5,6 +5,24 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+description = """
+This project implements a **highly secure file-sharing system** utilizing **FastAPI** and **MongoDB** for two distinct user roles: Operations (Ops) Users and Client Users.
+
+Key functionalities:
+- **Ops User**:
+  - Upload files restricted to `.pptx`, `.docx`, and `.xlsx` formats.
+  - Only authorized Ops Users are allowed to perform uploads.
+- **Client User**:
+  - Register with secure email verification flow.
+  - Access and download files via encrypted URLs.
+  - View a list of available files, ensuring controlled and secure access.
+
+Security highlights:
+- **JWT Authentication**: Ensures secure API access with role-based restrictions.
+- **Encrypted URLs**: Protects file downloads by generating secure, time-limited links.
+- **File Upload Restrictions**: Ensures only trusted file formats are uploaded, enhancing data security.
+"""
+
 BASE_URL = os.getenv("BASE_URL")
 
 SECRET_KEY = os.getenv("SECRET_KEY")
@@ -15,3 +33,7 @@ ENCRYPTION_KEY = utils.generate_key(SECRET_KEY)
 
 CONFIGURATION = Configuration()
 CONFIGURATION.api_key['api-key'] = os.getenv("BREVO_API_KEY")
+
+PROJECT_TITLE = "Secure File OPS"
+PROJECT_DESCRIPTION = description
+PROJECT_VERSION = "0.0.1"
--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 from src.auth.router import router as auth_router
 from src.file.router import router as file_router
+from src import config
 
-app = FastAPI()
+app = FastAPI(
+    title=config.PROJECT_TITLE,
+    description=config.PROJECT_DESCRIPTION,
+    version=config.PROJECT_VERSION,
+)
 
 app.include_router(auth_router, prefix="/auth")
 app.include_router(file_router, prefix="/files")
